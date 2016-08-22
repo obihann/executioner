19566f6233e0ae2074b3609d2ded56cfb05c2a99
Jeff Hann <jeff.hann@redspace.com>
2016-08-22 08:55:29 -0300

    fixing issues with init script and main script

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ let CucumberJSqTestScenario = require('./src/cucumberJSqTestScenario.js');
 let Utils = require('./src/utils.js');
 let cjsqts;
 let config;
-let screen = new Screen();
 
 /**
  * Start the executioner and select the correct actions.
@@ -23,43 +22,49 @@ let screen = new Screen();
  * @returns {undefined}
  */
 function execution (store) {
-    switch(process.argv[2]) {
-      case 'start':
-        cjsqts.parseAndConfigure(store);
-        break;
-      case 'finish':
-        cjsqts.processAndSubmit(store);
-        break;
-      case 'full':
-        cjsqts.parseAndConfigure(store).then( x => {
-          cjsqts.processAndSubmit(x);
-        });
-        break;
-      case 'init':
-        init();
-        break;
-      default:
-        cjsqts.sendHelp();
-        break;
-    }
+  switch(process.argv[2]) {
+    case 'start':
+      cjsqts.parseAndConfigure(store);
+      break;
+    case 'finish':
+      cjsqts.processAndSubmit(store);
+      break;
+    case 'full':
+      cjsqts.parseAndConfigure(store).then( x => {
+        cjsqts.processAndSubmit(x);
+      });
+      break;
+    case 'init':
+      init();
+      break;
+    default:
+      cjsqts.sendHelp();
+      break;
+  }
 }
 
 let path = `${__dirname}/../../executioner.conf`;
 path = typeof argv.config === 'undefined' ? path : argv.config;
 
-// Load config.json
-Utils.loadJSON(path).then(data => {
-  config = data;
-  config.dataFile = TESTRUN;
-  cjsqts = new CucumberJSqTestScenario(argv, config, screen);
-
-  // Check for token and host
-  Utils.isDefined(config.tracker, 'qTest Scenario token is unset.');
-  Utils.isDefined(config.host,'Host ID is unset.' );
-
-  return Utils.loadDataFile(config.dataFile);
-}).then(execution).catch(
-() => {
-  screen.screen.destroy();
+if (process.argv[2] === 'init') {
   init();
-});
+} else {
+  let screen = new Screen();
+
+  // Load config.json
+  Utils.loadJSON(path).then(data => {
+    config = data;
+    config.dataFile = TESTRUN;
+    cjsqts = new CucumberJSqTestScenario(argv, config, screen);
+
+    // Check for token and host
+    Utils.isDefined(config.tracker, 'qTest Scenario token is unset.');
+    Utils.isDefined(config.host,'Host ID is unset.' );
+
+    return Utils.loadDataFile(config.dataFile);
+  }).then(execution).catch(
+    e => {
+      screen.screen.destroy();
+      throw new Error(e);
+    });
+}
